198870455adc0def406b816c90920f5d69efa405
Rowan Cockett <rowanc1@gmail.com>
2022-10-20 12:59:09 -0600

    🧪 Move objects.inv out of the static folder

--- theme/app/utils/loaders.server.ts
+++ theme/app/utils/loaders.server.ts
@@ -58,7 +58,7 @@ export async function getPage(
 }
 
 export async function getObjectsInv(): Promise<Buffer | null> {
-  const url = updateLink('/_static/objects.inv');
+  const url = updateLink('/objects.inv');
   console.log(url);
   const response = await fetch(url).catch(() => null);
   if (!response || response.status === 404) return null;
