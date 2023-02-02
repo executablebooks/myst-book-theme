48f2bc29a37acf8641ecde2c8d5552fefa359693
Rowan Cockett <rowanc1@gmail.com>
2023-02-01 21:12:17 -0700

    💃 Use thebe-core

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,24 +1,13 @@
 import { RemixBrowser } from '@remix-run/react';
-import { Provider } from 'react-redux';
 import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
-import type { types } from '@curvenote/runtime';
-import { register } from '@curvenote/components';
-import { host } from '@curvenote/connect';
-import { createCurvenoteReduxStore } from '@myst-theme/site';
 
 function hydrate() {
-  const store = createCurvenoteReduxStore();
-  register(store as types.Store);
-  host.registerMessageListener(store);
-  (window as any).store = store;
   startTransition(() => {
     hydrateRoot(
       document,
       <StrictMode>
-        <Provider store={store}>
-          <RemixBrowser />
-        </Provider>
+        <RemixBrowser />
       </StrictMode>
     );
   });
