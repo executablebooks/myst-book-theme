5b6faa77e34a0f8e10f3f5ddcf1434f1dd22fed6
stevejpurves <steve@curvenote.com>
2023-04-26 16:21:02 -0600

    🧾added TODO

--- app/routes/$project.tsx
+++ app/routes/$project.tsx
@@ -1,6 +1,9 @@
 import { ProjectPageCatchBoundary } from '@myst-theme/site';
 import Page, { ArticlePageAndNavigation } from './$project.$slug';
 export { loader, links } from './$project.$slug';
+
+// TODO render something that can give us a ProjectContext
+// around the Page
 export default Page;
 
 export function CatchBoundary() {
